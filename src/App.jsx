cc685ff4406accbe8e3c631b2dc7512722fb5658
muhammadfuaad <fuaad29@gmail.com>
2024-01-29 21:35:14 +0500

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = (false)
+  const [charAllowed, setCharAllowed] = (false)
+  const [password, setPassword] = ("")
+  const passwordGenerator = useCallback(()=>{
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "01234567890"
+    if (charAllowed) str += "!@#$%^&*()_+-={}[]|"
+
+    for(let i=1; i<array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+    }
+    setPassword = pass
+  }, [length, numberAllowed, charAllowed, password])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="range" 
+        min={6} 
+        max={8} 
+        value={length}
+        onChange={(e)=>{setLength(e.target.value)}}
+      />
+      <label>Length: {length}</label>
+      <input type='checkbox'
+      defaultChecked={numberAllowed}
+      id = "numberInput"
+      onChange={()=>{setNumberAllowed(prev=>!prev)}}
+      />
     </>
   )
 }
 
 export default App
+ 
